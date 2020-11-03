f26b16adf963fe5a805e31796c888b47a935f279
yulinying <yulinying_1994@outlook.com>
2020-11-03 10:13:59 +0800

    add spring cloud config sample

---
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/site/assad/demo/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package site.assad.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+    
+}
